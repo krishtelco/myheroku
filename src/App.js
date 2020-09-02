8c66d550dfaab66ba89231cedb3c8b683304e723
krishtelco <krishtelco@gmail.com>
2020-09-02 10:22:49 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -7,28 +7,12 @@ import FormLogin from "./FormLogin";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="App">
-        <Container className="d-flex vertali">
-          <Row className="pad1 border border-yellow rowback">
-            <p>Inside PARA</p>
-            <FormLogin />
-          </Row>
-        </Container>
-      </div>
+      <Container className="d-flex vertali">
+        <Row className="pad1 border border-yellow rowback">
+          <p>Inside PARA</p>
+          <FormLogin />
+        </Row>
+      </Container>
     </div>
   );
 }
