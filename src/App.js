895fb56f79b7545dcd624e0d85cfab29b7e26f4b
krishtelco <krishtelco@gmail.com>
2020-09-02 10:13:07 +0530

    loginform

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Container, Row } from "react-bootstrap";
+import FormLogin from "./FormLogin";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Container className="d-flex vertali">
           <Row className="pad1 border border-yellow rowback">
             <p>Inside PARA</p>
+            <FormLogin />
           </Row>
         </Container>
       </div>
